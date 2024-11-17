115a6b2df38e3a319c6dafd5d5bfe35267991789
ogone4ek2090 <ahmtimur30@gmail.com>
2024-11-18 00:04:47 +0500

    fixes && create Database class

--- commands.py
+++ commands.py
@@ -5,7 +5,7 @@ import time
 
 from playsound import playsound
 from gtts import gTTS
-from database import add_request, get_last_request, init_tables, get_last_num
+from database import Database
 
 
 AVALIABLE = set(map(str, range(10))) | {"*", "+", "/", "-", "."}
@@ -29,54 +29,57 @@ sr.pause_threshold = 0.5
 
 
 def listening(self):
-    init_tables()
-    with speech_recognition.Microphone() as mic:
-        while True:
-            if self.state == 1:
+    db = Database()
+    db.init_tables()
+    while True:
+        if self.state == 1:
+            with speech_recognition.Microphone() as mic:
                 sr.adjust_for_ambient_noise(mic, duration=0.5)
                 data = sr.listen(mic)
 
-                try:
-                    text = sr.recognize_google(data, language="ru").lower().replace("х", "*")
-                    start = time.time()
-                except speech_recognition.exceptions.UnknownValueError:
-                    end = time.time()
-                    if end - start > 10:
-                        self.listen_handler()
-                    continue
+            try:
+                text = sr.recognize_google(data, language="ru").lower().replace("х", "*")
+                start = time.time()
+            except speech_recognition.exceptions.UnknownValueError:
+                end = time.time()
+                if end - start > 10:
+                    self.listen_handler()
+                continue
 
-                if text in ["привет", "hi"]:
-                    say("Здравствуйте")
+            if text in ["привет", "hi"]:
+                say("Здравствуйте")
 
-                elif text in ["открой youtube", "open youtube"]:
-                    webbrowser.open("https://youtube.com")
-                    say("Выполнено")
-                    add_request(text, "Выполнено")
+            elif text in ["открой youtube", "open youtube"]:
+                webbrowser.open("https://youtube.com")
+                say("Выполнено")
+                db.add_request(text, "Выполнено")
 
-                elif text in ["выключись", "turn off"]:
-                    say("Выключаюсь")
-                    add_request(text, "Выключаюсь")
-                    self.stop_handler()
-                    sys.exit()
+            elif text in ["выключись", "turn off"]:
+                say("Выключаюсь")
+                db.add_request(text, "Выключаюсь")
+                self.stop_handler()
+                sys.exit()
 
-                elif text in ["повтори", "repeat"]:
-                    last_saied = get_last_request()
-                    say(last_saied[1])
+            elif text in ["повтори", "repeat"]:
+                last_saied = db.get_last_request()
+                say(last_saied[1])
 
+            else:
+                last_num = db.get_last_num[0]
+                text = text\
+                    .replace(",", ".")\
+                    .replace("в степени", "**")\
+                    .replace(" ", "")\
+                    .replace("последняя", last_num)\
+                    .replace("последние", last_num)
 
-                else:
-                    text = text.replace(",", ".")\
-                        .replace("в степени", "**")\
-                        .replace(" ", "")\
-                        .replace("последняя", get_last_num()[0])\
-                        .replace("последние", get_last_num()[0])
-
-                    if check_str(text):
+                if check_str(text):
                         num = str(eval(text))
                         say(num)
-                        add_request(text, num, True)
-                    else:
-                        say("Не поняла вас")
-                        print(f"Фраза не распознана: {text}")
+                        db.add_request(text, num, True)
+
+                else:
+                    say("Не поняла вас")
+                    print(f"Фраза не распознана: {text}")
             if self.stop == 1:
                 sys.exit()
--- database.py
+++ database.py
@@ -1,45 +1,43 @@
 import sqlite3
 
 
-def init_tables():
-    conn = sqlite3.connect("database.db")
-    conn.execute("""
-    CREATE TABLE IF NOT EXISTS requests (
-        id INTEGER PRIMARY KEY, 
-        text text NOT NULL,
-        answer text NOT NULL,
-        is_num BOOL NOT NULL
-    );
-    """)
+class Database:
+    def __init__(self) -> None:
+        self.connection = sqlite3.connect("database.db")
+
+    def init_tables(self):
+        self.connection.execute("""
+        CREATE TABLE IF NOT EXISTS requests (
+            id INTEGER PRIMARY KEY, 
+            text text NOT NULL,
+            answer text NOT NULL,
+            is_num BOOL NOT NULL
+        );
+        """)
 
 
-def add_request(text, answer, is_num=False):
-    conn = sqlite3.connect("database.db")
-    conn.execute(f"""
-    INSERT INTO requests (text, answer, is_num) VALUES ('{text}', '{answer}', {str(is_num).lower()});
-    """)
-    conn.commit()
+    def add_request(self, text, answer, is_num=False):
+        self.connection.execute(f"""
+        INSERT INTO requests (text, answer, is_num) VALUES ('{text}', '{answer}', {str(is_num).lower()});
+        """)
+        self.connection.commit()
 
 
-def get_last_request():
-    conn = sqlite3.connect("database.db")
-    cursor = conn.cursor()
-    cursor.execute("""
-    SELECT text, answer FROM requests ORDER BY 0 - id LIMIT 1
-    """)
-    return cursor.fetchone()
+    def get_last_request(self):
+        cursor = self.connection.cursor()
+        cursor.execute("""
+        SELECT text, answer FROM requests ORDER BY 0 - id LIMIT 1
+        """)
+        result = cursor.fetchone()
+        cursor.close()
+        return result
 
 
-def get_last_num():
-    conn = sqlite3.connect("database.db")
-    cursor = conn.cursor()
-    cursor.execute("""
-    SELECT answer FROM requests WHERE is_num = true ORDER BY 0 - id LIMIT 1
-    """)
-    return cursor.fetchone()
-
-
-if __name__ == "__main__":
-    init_tables()
-    #add_request("test", "test2", False)
-    print(get_last_num())
+    def get_last_num(self):
+        cursor = self.connection.cursor()
+        cursor.execute("""
+        SELECT answer FROM requests WHERE is_num = true ORDER BY 0 - id LIMIT 1
+        """)
+        result = cursor.fetchone()
+        cursor.close()
+        return result
