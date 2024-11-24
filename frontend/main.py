01dc4cdd84874050a89f911557635a0c26f15652
ogone4ek2090 <ahmtimur30@gmail.com>
2024-11-25 02:16:38 +0500

    update

--- frontend/main.py
+++ frontend/main.py
@@ -2,12 +2,17 @@ import sys
 
 from PyQt6.QtWidgets import QApplication
 from forms.loginregister import LoginWindow
+from forms.helperog import Helperog
 
 
 def main():
     app = QApplication(sys.argv)
-    login = LoginWindow(app)
-    login.show()
+    if sys.argv[-1] == "test":
+        login = LoginWindow(app)
+        login.show()
+    else:
+        helperog = Helperog(app)
+        helperog.show()
     sys.exit(app.exec())
 
 
